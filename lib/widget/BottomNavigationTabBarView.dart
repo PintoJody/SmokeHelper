3157bdf5095e9f994be9134c49ddb21ff9a22a73
Pinto Jody <pintojody20@gmail.com>
2023-02-13 11:27:06 +0100

    add index to icon

--- lib/widget/BottomNavigationTabBarView.dart
+++ lib/widget/BottomNavigationTabBarView.dart
@@ -1,37 +1,83 @@
 import 'package:flutter/material.dart';
 
-class BottomNavigationTabBarView extends StatelessWidget {
+class BottomNavigationTabBarView extends StatefulWidget {
+  const BottomNavigationTabBarView({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationTabBarView> createState() => _BottomNavigationTabBarViewState();
+}
+
+class _BottomNavigationTabBarViewState extends State<BottomNavigationTabBarView> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return bottomNavigationTabBarView();
-  }
-
-  BottomNavigationBar bottomNavigationTabBarView() {
-    const iconSize = 25.0;
     return BottomNavigationBar(
-
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Accueil',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Journal',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart),
-            label: 'Progrès',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.emoji_events),
-            label: 'Classement',
-          ),
-        ]
-    );
+          currentIndex: _currentIndex,
+          onTap: (int newIndex){
+            setState(() {
+              _currentIndex = newIndex;
+            });
+          },
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Accueil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Journal',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: 'Progrès',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emoji_events),
+              label: 'Classement',
+            ),
+          ]
+      );
+    }
   }
-}
+
+
+
+// class BottomNavigationTabBarView extends State<BottomNavigationTabBarView> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return bottomNavigationTabBarView();
+//   }
+//
+//   BottomNavigationBar bottomNavigationTabBarView() {
+//     const iconSize = 25.0;
+//     return BottomNavigationBar(
+//
+//         selectedItemColor: Colors.green,
+//         unselectedItemColor: Colors.grey,
+//         type: BottomNavigationBarType.fixed,
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Accueil',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.book),
+//             label: 'Journal',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.bar_chart),
+//             label: 'Progrès',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.emoji_events),
+//             label: 'Classement',
+//           ),
+//         ]
+//     );
+//   }
+// }
