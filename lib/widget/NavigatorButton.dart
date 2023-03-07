6b3df91dae4e248cab32ff96de930cdbacd15902
Pinto Jody <pintojody20@gmail.com>
2023-03-07 10:04:54 +0100

    add CardBadge + ActionButton Widgets

--- lib/widget/NavigatorButton.dart
+++ lib/widget/NavigatorButton.dart
@@ -5,12 +5,13 @@ import '../theme/theme.dart';
 class NavigatorButton extends StatefulWidget {
   final BuildContext parentContext;
   final String text;
+  final double textSize;
   final Color bgColor;
   final double width;
   final double height;
   final double radius;
 
-  const NavigatorButton({required this.parentContext, required this.text, this.bgColor = CustomTheme.greenColor, this.width = 100.0, this.height = 25.0, this.radius = 15.0});
+  const NavigatorButton({required this.parentContext, required this.text, this.textSize = 12.0, this.bgColor = CustomTheme.greenColor, this.width = 100.0, this.height = 25.0, this.radius = 15.0});
 
   @override
   State<NavigatorButton> createState() => _NavigatorButtonState();
@@ -30,7 +31,12 @@ class _NavigatorButtonState extends State<NavigatorButton> {
         onPressed: () {
           Navigator.pushNamed(widget.parentContext, '/');
         },
-        child: Text(widget.text),
+        child: Text(
+            widget.text,
+          style: TextStyle(
+            fontSize: widget.textSize
+          ),
+        ),
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.transparent,
           shadowColor: Colors.transparent,
