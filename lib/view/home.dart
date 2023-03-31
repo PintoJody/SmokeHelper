195f2ff2e68d838954708265f55d1af63ee21c03
Pinto Jody <pintojody20@gmail.com>
2023-03-31 11:02:06 +0200

    :white_check_mark: HomeCardInfo OK

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:smoke_helper/widget/CardWithBadgesUnlocked.dart';
@@ -6,7 +7,9 @@ import 'package:smoke_helper/widget/CardWithUserInfo.dart';
 import 'package:smoke_helper/widget/imgWithText.dart';
 
 import '../model/BadgeModel.dart';
+import '../service/getCigUserByIdService.dart';
 import '../service/getUserBySlug.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,13 +21,26 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   bool _isLoading = true;
   int _countBadges = 0;
+  int _thisMonth = 0;
+  int _lastMonth = 0;
+  final CigUserService _getCigUserService = CigUserService();
   List<Badge> _badges = [];
+  int _daysDifference = 0;
+  int _monthsDifference = 0;
+  int _hoursDifference = 0;
+  int _minutesDifference = 0;
+  int _secondsDifference = 0;
+  num _savedMonthly=0;
+  num _lessCigsMonthly=0;
+  dynamic _cigInfo=null;
 
   @override
   void initState() {
     super.initState();
     getUserData();
     _fetchBadges();
+    _getLastCig();
+    _getCigUser();
   }
 
   Future<void> getUserData() async {
@@ -36,8 +52,55 @@ class _HomePage extends State<HomePage> {
     setState(() {
       _isLoading = false;
       _countBadges = responseData["badges"].length;
+      _cigInfo = responseData["cigInfo"];
+    });
+  }
 
-      print(_countBadges);
+  Future<void> _getLastCig() async {
+    final responseJson = await CigUserService.getLastCig();
+
+    Map<String, dynamic> responseData;
+    responseData = await json.decode(responseJson.data);
+
+    setState(() {
+      _isLoading = false;
+
+      //Datetime without smoke
+      final now = DateTime.now();
+      final responseDate = DateTime.parse(responseData['createdAt']);
+
+      _daysDifference = now.difference(responseDate).inDays;
+      _monthsDifference = ((_daysDifference % 365) / 30).floor();
+      _hoursDifference = now.difference(responseDate).inHours;
+      _minutesDifference = now.difference(responseDate).inMinutes;
+      _secondsDifference = now.difference(responseDate).inSeconds;
+
+    });
+  }
+
+  Future<void> _getCigUser() async {
+    final responseJson = await _getCigUserService.getCigUser();
+
+    Map<String, dynamic> responseData;
+    responseData = await json.decode(responseJson.data);
+
+    setState(() {
+      _isLoading = false;
+      _thisMonth = responseData["monthly"]["thisMonth"].length;
+      _lastMonth = responseData["monthly"]["lastMonth"].length;
+      _savedMonthly=(_lastMonth / _cigInfo["cigsPerPack"]).ceil() * _cigInfo["averagePackPrice"]-(_thisMonth / _cigInfo["cigsPerPack"]).ceil() * _cigInfo["averagePackPrice"];
+      _lessCigsMonthly=_lastMonth-_thisMonth;
+
+      //Cig avoided
+      if(_lessCigsMonthly < 0){
+        _lessCigsMonthly = 0;
+      }
+
+      //money saved
+      if(_savedMonthly < 0){
+        _savedMonthly = 0;
+      }
+
     });
   }
 
@@ -46,8 +109,6 @@ class _HomePage extends State<HomePage> {
 
     Map<String, dynamic> responseData;
     responseData = await json.decode(responseJson.data);
-
-    print(responseData["badges"]);
 
     setState(() {
       _badges = responseData["badges"]
@@ -60,7 +121,6 @@ class _HomePage extends State<HomePage> {
           ))
           .toList();
     });
-
   }
 
   @override
@@ -74,9 +134,9 @@ class _HomePage extends State<HomePage> {
       Icons.emoji_events_outlined,
     ];
     final List<String> descriptionsProgres = [
-      '2 mois sans fumer',
-      '324 cigarettes évitées',
-      '102€ économisés',
+      '$_daysDifference jours sans fumer',
+      '$_lessCigsMonthly cigarettes évitées',
+      '${_savedMonthly}€ économisés',
       '$_countBadges badges obtenus',
     ];
     //TODO voir condition dans CardWithItems car on utilise des icons pour progres mais des images pour les badges
@@ -87,6 +147,7 @@ class _HomePage extends State<HomePage> {
       0xFF4ECB71,
     ];
 
+
     return _isLoading ? Center(child: CircularProgressIndicator()) : SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.only(top: 25.0, left: 9, right: 9),
@@ -95,7 +156,7 @@ class _HomePage extends State<HomePage> {
             imgWithText(
                 title: 'Temps passé sans fumer :',
                 img: 'https://img.freepik.com/vecteurs-libre/illustration-scenes-nature-dessinees-main_23-2150053464.jpg?w=740&t=st=1676362568~exp=1676363168~hmac=477a3622922208d1a59f7390ee35fcd96351ec687b0706b8cfce2ffe749defca',
-                detail: '2m 12j 5h'
+                detail: '${_monthsDifference}m ${_daysDifference}j ${_hoursDifference}h'
             ),
             const SizedBox(height: 40.0),
             CardWithUserInfo(
